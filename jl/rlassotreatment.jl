471db3b053b0e6d37b526d71163160a613ef3521
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2022-08-22 13:00:49 -0500

    rlassoLATE

--- jl/rlassotreatment.jl
+++ jl/rlassotreatment.jl
@@ -1,7 +1,7 @@
-using GLM
-using Distributionss
+using GLM, Statistics
+using Distributions, Random
 
-include("rlasso.jl")
+include("hdmjl.jl")
 include("rlassologit.jl")
 
 using CSV, DataFrames
@@ -15,24 +15,42 @@ d = dlate[:, 2]
 y = ylate[:, 3]
 z = zlate[:, 1]
 
+rlassologit(Matrix(x), d)
+
+rlasso(Matrix(x)[:, [3, 4]], x[:, 2])["coefficients"]
+
+
 ### params
 #### x, d, y, z
 
-# bootstrap = "none"
-# # n_rep = 5 #500
-# post = true
-# intercept = true
+bootstrap = "none"
+n_rep = 5 #500
+post = true
+intercept = true
 # always_takers = true
 # never_takers = true
 
-function rlassoLATE(x, d, y, z; boostrap = "none", n_rep = 100, always_takers = true, never_takers = true)
+function rlassoLATE(x, d, y, z; bootstrap = "none", n_rep = 100, always_takers = true, 
+    post = true, intercept = true, never_takers = true)
 
     ## rlassoLATE with intercept 
     ## bootstrap missing
     # intercept = true
 
+
     x1 = Matrix(x)
     n, p = size(x1)
+
+    function get_mtrx(x1, type = intercept)
+        n1, p1 = size(x1)
+        if type
+            x_mtrx = hcat(ones(n1), x1)
+        else
+            x_mtrx = x1
+        end
+        return x_mtrx
+    end
+
 
     if bootstrap ∉ ["none", "Bayes", "normal", "wild"]
         error("No valid bootstrap")
@@ -59,40 +77,45 @@ function rlassoLATE(x, d, y, z; boostrap = "none", n_rep = 100, always_takers = 
     end
 
 
-    x_z1 = Matrix(x[indz1, :])
+    x_z1 = Matrix(x1[indz1, :])
     y_z1 = y[indz1, :]
 
-    x_z0 = Matrix(x[indz0, :])
+    x_z0 = Matrix(x1[indz0, :])
     y_z0 = y[indz0, :]
 
 
-    include("help_functions.jl")
-    include("rlasso.jl")
-    include("LassoShooting_fit.jl")
-
+    # include("help_functions.jl")
+    # include("hdmjl.jl")
+    # include("LassoShooting_fit.jl")
+    
+    # init_values(x_z1, y_z1)
+    
     b_y_z1xL = rlasso(
         x_z1, y_z1, post = post, intercept = intercept,
         tol = ctrl_tol, maxIter = ctrl_num_iter, #controls
-        homoskedastic = penalty_homoscedastic, c = penalty_c, gamma = penalty_gamma #penalty
+        homoskedastic = penalty_homoscedastic, c = penalty_c, gamma = penalty_gamma, #penalty
+        lambda_start = penalty_lambda_start
         # lambda start not found => via Lambda calculation
     )
 
     ### intercept with main X
-    my_z1x = hcat(ones(size(x_z1, 1)), x_z1) * b_y_z1xL["coefficients"][:, 2]
+    # b_y_z1xL["coefficients"]
+    my_z1x = get_mtrx(x1) * b_y_z1xL["coefficients"]
 
     b_y_z0xL = rlasso(
         x_z0, y_z0, post = post, intercept = intercept,
         tol = ctrl_tol, maxIter = ctrl_num_iter, #controls
-        homoskedastic = penalty_homoscedastic, c = penalty_c, gamma = penalty_gamma #penalty
+        homoskedastic = penalty_homoscedastic, c = penalty_c, gamma = penalty_gamma, #penalty
+        lambda_start = penalty_lambda_start
         # lambda start not found => via Lambda calculation
     )
 
     ## with intercept
-    my_z0x = hcat(ones(size(x_z0, 1)), x_z0) * b_y_z0xL["coefficients"][:, 2]
+    my_z0x = get_mtrx(x1) * b_y_z0xL["coefficients"]
 
     lambda = 2.2 * sqrt(n) * quantile(Normal(0, 1), 1 - (.1 / log(n)) / (2 * (2 * p)))
 
-    include("rlassologit.jl")
+    # include("rlassologit.jl")
 
     if z == d
         md_z1x = ones(n)
@@ -103,26 +126,26 @@ function rlassoLATE(x, d, y, z; boostrap = "none", n_rep = 100, always_takers = 
         if all([always_takers, never_takers])
 
             g_d_z1 = rlassologit(x_z1, d[indz1], post = post, intercept = intercept, penalty_lambda = lambda, penalty_c = penalty_c, penalty_gamma = penalty_gamma)
-            yp1 = hcat(ones(size(x, 1)), Matrix(x)) * g_d_z1["coefficients"]
+            yp1 = get_mtrx(x1) * g_d_z1["coefficients"]
             md_z1x = 1 ./ (1 .+ exp.(-1 .* (yp1)))
 
             g_d_z0 = rlassologit(x_z0, d[indz0], post = post, intercept = intercept, penalty_lambda = lambda, penalty_c = penalty_c, penalty_gamma = penalty_gamma)
-            yp0 = hcat(ones(size(x, 1)), Matrix(x)) * g_d_z0["coefficients"]
+            yp0 = get_mtrx(x1) * g_d_z0["coefficients"]
             md_z0x = 1 ./ (1 .+ exp.(-1 .* (yp0)))
 
         elseif always_takers == false & never_takers == true
             g_d_z1 = rlassologit(x_z1, d[indz1], post = post, intercept = intercept, penalty_lambda = lambda, penalty_c = penalty_c, penalty_gamma = penalty_gamma)
-            yp1 = hcat(ones(size(x1, 1)), Matrix(x)) * g_d_z1["coefficients"]
+            yp1 = get_mtrx(x1) * g_d_z1["coefficients"]
             md_z1x = 1 ./ (1 .+ exp.(-1 .* (yp0)))
 
             md_z0x = zeros(size(x1, 1))
 
-        elseif always_takers == true never_takers == false 
+        elseif always_takers == true & never_takers == false 
 
             md_z1x = ones(size(x, 1))
 
             g_d_z0 = rlassologit(x_z0, d[indz0], post = post, intercept = intercept, penalty_lambda = lambda, penalty_c = penalty_c, penalty_gamma = penalty_gamma)
-            yp0 = hcat(ones(size(x, 1)), Matrix(x)) * g_d_z0["coefficients"]
+            yp0 = get_mtrx(x1) * g_d_z0["coefficients"]
             md_z0x = 1 ./ (1 .+ exp.(-1 .* (yp0)))
         elseif always_takers == false & never_takers == false
             md_z0x = zeros(n)
@@ -132,21 +155,23 @@ function rlassoLATE(x, d, y, z; boostrap = "none", n_rep = 100, always_takers = 
 
     ## intercept
 
-    b_z_xl = rlassologit(x, z, post = post, intercept = intercept)
-    yp_b = hcat(ones(size(x, 1)), Matrix(x)) * b_z_xl["coefficients"]
+    b_z_xl = rlassologit(x1, z, post = post, intercept = intercept)
+    yp_b = get_mtrx(x1) * b_z_xl["coefficients"]
     mz_x = 1 ./ (1 .+ exp.(-1 .* (yp_b)))
 
     mz_x = mz_x .* (mz_x .> 1e-12 .&& mz_x .< (1 - 1e-12)) .+ (1 - 1e-12) .* (
         mz_x .> 1 .- 1e-12) .+ 1e-12 .* (mz_x .< 1e-12)
+    
+    # @. z * (y - my_z1x) / mz_x - ((1- z) * (y - my_z0x))
 
-    eff <- @. (
+    eff = @. (
         z * (y - my_z1x) / mz_x - ((1 - z) * (y - my_z0x)/(1 - mz_x)) + 
         my_z1x - my_z0x) / mean(z * (d - md_z1x)/mz_x - ((1 - z) * (d - md_z0x)/(1 - mz_x)) + 
         md_z1x - md_z0x
         )
 
     se = sqrt(var(eff)) / sqrt(n)
-    late = mean(se)
+    late = mean(eff)
     individual = eff
 
     object = Dict(
@@ -154,8 +179,34 @@ function rlassoLATE(x, d, y, z; boostrap = "none", n_rep = 100, always_takers = 
         "sample_size" => n
     )
 
+    boot = []
+    if bootstrap != "none"
+        for i in 1:n_rep
+            if bootstrap == "Bayes"
+                method = Exponential()
+                weights = rand(method, n) .- 1
+            elseif bootstrap == "normal"
+                method = Normal()
+                weights = rand(method, n) 
+            else
+                method = Normal()
+                weights = rand(method, n) ./ sqrt(2) .+ (rand(method, n).^2 .- 1) ./ 2
+            end
+            weights = weights .+ 1
+
+            bt =  mean(@. weights * (z * (y - my_z1x)/mz_x - ((1 - z) * (y - my_z0x)/(1 - mz_x)) + my_z1x - my_z0x))/mean(@. weights * (z * (d - md_z1x)/mz_x - ((1 - z) * (d - md_z0x)/(1 - mz_x)) + md_z1x - md_z0x))
+            append!(boot, bt)
+        end
+        object["boot_se"] = sqrt(var(boot))
+        object["type_boot"] = bootstrap
+        object["boot_n"] = n_rep
+    end
+    object
+    # boot
+
     return object
 end
 
 
+rlassoLATE(x, d, y, z, bootstrap = "wild", always_takers = true, never_takers = true, intercept = false, post = false)
 
