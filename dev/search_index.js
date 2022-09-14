96358fcf67219cc643df70cf4aef3f836ca5ff9f
Documenter.jl <documenter@juliadocs.github.io>
2022-09-14 21:19:57 +0000

    build based on 138bfe8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HDMjl","category":"page"},{"location":"#HDMjl","page":"Home","title":"HDMjl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HDMjl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HDMjl]","category":"page"}]
+}
