b22a3e92b77544fdfba317428c2df25806d647ad
RodrigoGrijalba <ragrijalba119@gmail.com>
2022-09-14 16:24:38 -0500

    docs fix

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(;
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://d2cml-ai.github.io/HDMjl.jl",
-        edit_link="master",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
