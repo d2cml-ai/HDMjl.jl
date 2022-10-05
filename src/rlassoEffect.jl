9a694783b08714e363aa9633ceb57bebe1dd34e5
Franco Caceres Vasquez <franco.caceres@pucp.edu.pe>
2022-10-05 10:46:49 -0500

    #

--- src/rlassoEffect.jl
+++ src/rlassoEffect.jl
@@ -1,108 +1,165 @@
 
+mutable struct rlassoEffect1
+    se
+    sample_size
+    coefficients
+    dict
+end
+
 function rlassoEffect(
-        x, 
-        y, 
-        d;
-        method = "double selection",
-        I3 = nothing,
-        post = true
-    )
+    x, 
+    y, 
+    d;
+    method = "double selection",
+    I3 = nothing,
+    post = true
+)
 
-    #= x = Matrix(x)
-    y = Matrix(y[:, :])
-    d = Matrix(d[:, :]) =#
+#= x = Matrix(x)
+y = Matrix(y[:, :])
+d = Matrix(d[:, :]) =#
 
-    n, p = size(x)
+n, p = size(x)
 
-    # i3 = Set(I3)
+# i3 = Set(I3)
 
-    if method == "double selection"
-        I1 = rlasso(x, d, post = post)["index"]
-        I2 = rlasso(x, y, post = post)["index"]
+if method == "double selection"
+    I1 = rlasso(x, d, post = post)["index"]
+    I2 = rlasso(x, y, post = post)["index"]
 
-        if !isnothing(I3)
-            I = I1 + I2 + I3
-        else
-            I =  I1 + I2
-        end
-
-        if sum(I) == 0
-            I = nothing
-        end
-    
-        I[I .> 0] .= 1
-        I = BitVector(I)
-       
-        x = hcat(ones(n), d, x[:, I])
-        reg1 = GLM.lm(x, y)
-        alpha = GLM.coef(reg1)[2]
-    
-        xi = GLM.residuals(reg1) .* sqrt(n / (n - sum(I) - 1))
-    
-        if isnothing(I)
-            reg2 = GLM.lm(ones(n, 1), d)
-        else
-            reg2 = GLM.lm(x[:, Not(2)], d)
-        end
-    
-        v = GLM.residuals(reg2)
-    
-        var_r = 1 / n * 1 /mean(v.^2) * mean(v.^2 .* xi.^2) * 1 / mean(v.^2)
-    
-        se = sqrt(var_r)
-    
-        tval = alpha / se
-    
-        # pval = 2 ##3 searching function (`pnorm`)
-    
-       if isnothing(I)
-            no_select = 1
-        else
-            no_select = 0
-        end
-    
-        res = Dict(
-            "epsilon" => xi, "v" => v
-        )
-
-        results = Dict(
-            "alpha" => alpha, "se" => se, "t" => tval,
-            "no_select" => no_select, "coefficients" => alpha, "coefficient" => alpha,
-            "coefficients_reg" => GLM.coef(reg1), "selection_index" => I, "residuals" => res,
-            "sample_size" => n
-        )
-    elseif method == "partialling out"
-        reg1 = rlasso(x, y, post = post)
-        yr = reg1["residuals"]
-        reg2 = rlasso(x, d, post = post)
-        dr = reg2["residuals"]
-
-        data0 = hcat(yr, ones(n), dr)
-        reg3 = GLM.lm(data0[:, Not(1)], data0[:, 1])
-        alpha = GLM.coef(reg3)[2]
-
-        var = vcov(reg3)[2, 2]
-        se = sqrt.(var)
-        tval = alpha ./ sqrt(var)
-        # pval = 
-        res = Dict("epsilon" => GLM.residuals(reg3), "v" => dr)
-
-        I1 = reg1["index"]
-        I2 = reg2["index"]
-        I = as_logical(I1 + I2)
-        results = Dict(
-            "alpha" => alpha, "se" => se, "t" => tval, "coefficients" => alpha,
-            "coefficient" => alpha, "coefficients_reg" => reg1["coefficients"], "selection_index" => I,
-            "residuals" => res, "sample_size" => n
-        )
-
+    if !isnothing(I3)
+        I = I1 + I2 + I3
+    else
+        I =  I1 + I2
     end
 
-    return results
+    if sum(I) == 0
+        I = nothing
+    end
+
+    I[I .> 0] .= 1
+    I = BitVector(I)
+   
+    x = hcat(ones(n), d, x[:, I])
+    reg1 = GLM.lm(x, y)
+    alpha = GLM.coef(reg1)[2]
+
+    xi = GLM.residuals(reg1) .* sqrt(n / (n - sum(I) - 1))
+
+    if isnothing(I)
+        reg2 = GLM.lm(ones(n, 1), d)
+    else
+        reg2 = GLM.lm(x[:, Not(2)], d)
+    end
+
+    v = GLM.residuals(reg2)
+
+    var_r = 1 / n * 1 /mean(v.^2) * mean(v.^2 .* xi.^2) * 1 / mean(v.^2)
+
+    se = sqrt(var_r)
+
+    tval = alpha / se
+
+    # pval = 2 ##3 searching function (`pnorm`)
+
+   if isnothing(I)
+        no_select = 1
+    else
+        no_select = 0
+    end
+
+    res = Dict(
+        "epsilon" => xi, "v" => v
+    )
+
+    results = Dict(
+        "alpha" => alpha, "se" => se, "t" => tval,
+        "no_select" => no_select, "coefficients" => alpha, "coefficient" => alpha,
+        "coefficients_reg" => GLM.coef(reg1), "selection_index" => I, "residuals" => res,
+        "sample_size" => n
+    )
+elseif method == "partialling out"
+    reg1 = rlasso(x, y, post = post)
+    yr = reg1["residuals"]
+    reg2 = rlasso(x, d, post = post)
+    dr = reg2["residuals"]
+
+    data0 = hcat(yr, ones(n), dr)
+    reg3 = GLM.lm(data0[:, Not(1)], data0[:, 1])
+    alpha = GLM.coef(reg3)[2]
+
+    var = vcov(reg3)[2, 2]
+    se = sqrt.(var)
+    tval = alpha ./ sqrt(var)
+    # pval = 
+    res = Dict("epsilon" => GLM.residuals(reg3), "v" => dr)
+
+    I1 = reg1["index"]
+    I2 = reg2["index"]
+    I = as_logical(I1 + I2)
+    results = Dict(
+        "alpha" => alpha, "se" => se, "t" => tval, "coefficients" => alpha,
+        "coefficient" => alpha, "coefficients_reg" => reg1["coefficients"], "selection_index" => I,
+        "residuals" => res, "sample_size" => n
+    )
+
+end
+se = results["se"]
+sample_size = results["sample_size"]
+coefficient = results["coefficient"]
+return rlassoEffect1(se, sample_size, coefficient, results)
 
 end
 
+function r_summary(object::rlassoEffect1)
+    if length(object.coefficients) != 0
+        k = length(object.coefficients)
+        table = zeros(k, 4)
+        table[:, 1] .= object.coefficients
+        table[:, 2] .= object.se
+        table[:, 3] .= table[:, 1]./table[:, 2]
+        table[:, 4] .= 2 * cdf(Normal(), -abs.(table[:, 3]))
+        table1 = DataFrame(table, :auto)
+        table1 = rename(table1, ["Estimate.", "Std. Error", "t value", "Pr(>|t|)"])
+        print("""Estimates and significance testing of the effect of target variables""", 
+                "\n")
+        pretty_table(table, show_row_number = true, header = ["Estimate.", "Std. Error", "t value", "Pr(>|t|)"], tf = tf_borderless)
+        print("---", "\n", "Signif. codes:","\n", "0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1")
+        print("\n")
+    else
+        print("No coefficients\n")
+    end
+    return table1
+end
 
+function r_print(object::rlassoEffect1, digits = 3)
+    if length(object.coefficients) !=  0
+        b = ["X$y" for y = length(object.coefficients)]
+        b = reshape(b,(1,length(b)))
+        a = vcat(b, round.(object.coefficients', digits = 3))
+        if length(object.coefficients) <= 10
+            
+            println("Coefficients:\n")
+            pretty_table(a[2,:]', tf = tf_borderless, header = a[1,:])
+        else 
+            for i in 1:trunc(length(object.coefficients)/10)
+                pretty_table(a[2,10*(i-1)+1:10*i]', tf = tf_borderless, header = a[1,10*(i-1)+1:10*i])
+            end
+        pretty_table(a[2,10*trunc(length(object.coefficients)/10)+1:length(object.coefficients)]',
+                            tf = tf_borderless, header = a[1,10*trunc(length(object.coefficients)/10)+1:length(object.coefficients)])
+        end
+    else 
+        print("No coefficients\n")
+    end
+end
+
+mutable struct rlassoEffects1
+    se
+    sample_size
+    index
+    coefficients
+    dict
+end
 
 function rlassoEffects(x, y; index = 1:size(x, 2), I3 = nothing, method = "partialling out", post = true)
 
@@ -162,13 +219,13 @@ function rlassoEffects(x, y; index = 1:size(x, 2), I3 = nothing, method = "parti
         if lasso_reg[i] == "try-error"
             continue
         else
-            coefficients[i] = lasso_reg[i]["alpha"]
-            se[i] = lasso_reg[i]["se"]
-            t[i] = lasso_reg[i]["t"]
+            coefficients[i] = lasso_reg[i].dict["alpha"]
+            se[i] = lasso_reg[i].dict["se"]
+            t[i] = lasso_reg[i].dict["t"]
             # pval[i] = lasso_reg[i]["p_value"]
-            reside[:, i] = lasso_reg[i]["residuals"]["epsilon"]
-            residv[:, i] = lasso_reg[i]["residuals"]["v"]
-            coef_mat[i] = lasso_reg[i]["coefficients_reg"]
+            reside[:, i] = lasso_reg[i].dict["residuals"]["epsilon"]
+            residv[:, i] = lasso_reg[i].dict["residuals"]["v"]
+            coef_mat[i] = lasso_reg[i].dict["coefficients_reg"]
             # selection_matrix[Not(index[i]), i] = lasso_reg[i]["selection_index"]
         end
     end
@@ -179,7 +236,79 @@ function rlassoEffects(x, y; index = 1:size(x, 2), I3 = nothing, method = "parti
         "lasso_reg" => lasso_reg, "index" => index, "sample_size" => n,
         "residuals" => residuals, "coef_mat" => coef_mat, "selection_matrix" => selection_matrix
     )
+    se = res["se"]
+    sample_size = res["sample_size"]
+    index = res["index"]
+    coefficients = res["coefficients"]
+    return rlassoEffects1(se, sample_size, index, coefficients, res)
 
-    return res
+end
 
+function r_print(object::rlassoEffects1, digits = 3)
+    if length(object.coefficients) !=  0
+        b = ["X$y" for y = object.index]
+        b = reshape(b,(1,length(b)))
+        a = vcat(b, round.(object.coefficients', digits = digits))
+        if length(object.coefficients) <= 10
+            
+            println("Coefficients:\n")
+            pretty_table(a[2,:]', tf = tf_borderless, header = a[1,:])
+        else 
+            for i in 1:trunc(length(object.coefficients)/10)
+                pretty_table(a[2,10*(i-1)+1:10*i]', tf = tf_borderless, header = a[1,10*(i-1)+1:10*i])
+            end
+        pretty_table(a[2,10*trunc(length(object.coefficients)/10)+1:length(object.coefficients)]',
+                            tf = tf_borderless, header = a[1,10*trunc(length(object.coefficients)/10)+1:length(object.coefficients)])
+        end
+    else 
+        print("No coefficients\n")
+    end
+end
+
+function r_summary(object::rlassoEffects1)
+    if length(object.coefficients) != 0
+        k = length(object.coefficients)
+        table = zeros(k, 4)
+        table[:, 1] .= object.coefficients
+        table[:, 2] .= object.se
+        table[:, 3] .= table[:, 1]./table[:, 2]
+        table[:, 4] .= 2 * cdf(Normal(), -abs.(table[:, 3]))
+        table1 = DataFrame(hcat(["X$y" for y = object.index], table), :auto)
+        rename(table1, ["index", "Estimate.", "Std. Error", "t value", "Pr(>|t|)"])
+        print("Estimates and significance testing of the effect of target variables", 
+                "\n")
+        pretty_table(table, show_row_number = false, header = ["Estimate.", "Std. Error", "t value", "Pr(>|t|)"], tf = tf_borderless, row_names = ["X$y" for y = object.index])
+        print("---", "\n", "Signif. codes:","\n", "0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1")
+        print("\n")
+    else
+        print("No coefficients\n")
+    end
+end
+
+function r_confint(object::rlassoEffects1, level = 0.95)
+    n = object.sample_size
+    k = length(object.coefficients)
+    cf = object.coefficients
+    #pnames <- names(cf)
+    # if (missing(parm)) 
+    #     parm <- pnames else if (is.numeric(parm)) 
+    #       parm <- pnames[parm]
+    a = (1 - level)/2
+    a = [a, 1 - a]
+    fac = quantile.(Normal(), a)
+    pct = string.(round.(a; digits = 3)*100, "%")
+    ses = object.se
+    c_i = []
+    for i in 1:length(cf)
+        if i == 1
+            c_i = (cf[i] .+ ses[i] .* fac)[:,:]'
+        else
+            c_i = vcat(c_i, (cf[i] .+ ses[i] * fac)[:,:]')
+        end
+    end
+    table1 = DataFrame(hcat(["X$y" for y = object.index], c_i), :auto)
+    rename(table1, vcat("index", pct))
+    #ci = NamedArray(c_i, (1:size(c_i)[1], pct))
+    ci = pretty_table(c_i; header = pct, show_row_number = false, tf = tf_borderless, row_names = ["X$y" for y = object.index])
+    #return c_i;;
 end
--- src/rlassoIV.jl
+++ src/rlassoIV.jl
@@ -1,15 +1,23 @@
+mutable struct rlassoIV1
+    se
+    sample_size
+    #vcov
+    coefficients
+    dict
+end
+
 function rlassoIV(x, d, y, z; select_Z::Bool = true, select_X::Bool = true, post::Bool = true)
     if !select_Z & !select_X
         res = tsls(d, y, z, x, homoskedastic = false)
-        return res
+        se = res["se"]
         
     elseif select_Z & !select_X
         res = rlassoIVselectZ(x, d, y, z, post = post)
-        return res
+        res["sample_size"] = size(x)[1]
         
     elseif !select_Z & select_X
         res = rlassoIVselectX(x, d, y, z, post = post)
-        return res
+        res["sample_size"] = size(x)[1]
         
     elseif select_Z & select_X
         
@@ -45,6 +53,84 @@ function rlassoIV(x, d, y, z; select_Z::Bool = true, select_X::Bool = true, post
         end
         
         res = tsls(Dr, Yr, Zr, intercept = false, homoscedastic = false)
-        return res
     end
+    se = res["se"]
+    sample_size = res["sample_size"]
+    #vcov = res["vcov"]
+    coefficients = res["coefficients"]
+    res1 = rlassoIV1(se, sample_size, coefficients, res);
+    return res1;
+end
+
+function r_print(object::rlassoIV1, digits = 3)
+    if length(object.coefficients) !=  0
+        b = ["X$y" for y = length(object.coefficients)]
+        b = reshape(b,(1,length(b)))
+        a = vcat(b, round.(object.coefficients', digits = 3))
+        if length(object.coefficients) <= 10
+            
+            println("Coefficients:\n")
+            pretty_table(a[2,:]', tf = tf_borderless, header = a[1,:])
+        else 
+            for i in 1:trunc(length(object.coefficients)/10)
+                pretty_table(a[2,10*(i-1)+1:10*i]', tf = tf_borderless, header = a[1,10*(i-1)+1:10*i])
+            end
+        pretty_table(a[2,10*trunc(length(object.coefficients)/10)+1:length(object.coefficients)]',
+                            tf = tf_borderless, header = a[1,10*trunc(length(object.coefficients)/10)+1:length(object.coefficients)])
+        end
+    else 
+        print("No coefficients\n")
+    end
+end
+
+using Distributions
+function r_summary(object::rlassoIV1)
+    if length(object.coefficients) != 0
+        k = length(object.coefficients)
+        table = zeros(k, 4)
+        table[:, 1] .= object.coefficients
+        table[:, 2] .= object.se
+        table[:, 3] .= table[:, 1]./table[:, 2]
+        table[:, 4] .= 2 * cdf(Normal(), -abs.(table[:, 3]))
+        table1 = DataFrame(table, :auto)
+        table1 = rename(table1, ["coeff.", "se.", "t-value", "p-value"])
+        print("Estimates and Significance Testing of the effect of target variables in the IV regression model", 
+                "\n")
+        pretty_table(table, show_row_number = true, header = ["coeff.", "se.", "t-value", "p-value"], tf = tf_borderless)
+        print("---", "\n", "Signif. codes:","\n", "0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1")
+        print("\n")
+        return table1;
+    else
+        print("No coefficients\n")
+        #table = []
+    end
+    #return table;;
+end
+
+function r_confint(object::rlassoIV1, level = 0.95)
+    n = object.sample_size
+    k = length(object.coefficients)
+    cf = object.coefficients
+    #pnames <- names(cf)
+    # if (missing(parm)) 
+    #     parm <- pnames else if (is.numeric(parm)) 
+    #       parm <- pnames[parm]
+    a = (1 - level)/2
+    a = [a, 1 - a]
+    fac = quantile.(Normal(), a)
+    pct = string.(round.(a; digits = 3)*100, "%")
+    ses = object.se
+    c_i = []
+    for i in 1:length(cf)
+        if i == 1
+            c_i = (cf[i] .+ ses[i] .* fac)[:,:]'
+        else
+            c_i = vcat(c_i, (cf[i] .+ ses[i] .* fac)[:,:]')
+        end
+    end
+    table1 = DataFrame(c_i, :auto)
+    table1 = rename(table1, pct)
+    #ci = NamedArray(c_i, (1:size(c_i)[1], pct))
+    ci = pretty_table(c_i; header = pct, show_row_number = true, tf = tf_borderless)
+    return table1;
 end
