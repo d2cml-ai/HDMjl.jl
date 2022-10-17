1ee534bd4c374ba343d99c5eb8a374bf4de70efb
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2022-10-17 11:38:48 -0500

    Paper

---
+++ tutorial/hdm_build.jl
@@ -0,0 +1,19 @@
+### Instrucciones para crear el pdf
+
+# - Tener `pandoc`
+# - Tener `Latex`
+# - Instalar los siguientes paquetes `] add Weave Highlights`
+# - Ir a la carpeta principal (meidante julia, bash o VSCode)
+#     - asegurarse que se esta trabajando con el entorno general β`] activate`
+# - correr en el bash o la terminal de vscode `julia test/hdm_build.jl`
+
+########## Code 
+
+using Weave, Highlights
+
+# @time weave("hdm.jmd"; doctype = "md2pdf", highlight_theme = Highlights.Themes.MonokaiMiniTheme, pandoc_options = ["--toc", "--toc-depth= 3", "--number-sections", "--self-contained"])
+# @time weave("hdm.jmd"; doctype="pandoc2pdf", pandoc_options=["--toc", "--toc-depth= 1", "--number-sections", "--self-contained"], highlight_theme = Highlights.Themes.TangoTheme)
+@time weave("test/hdm.jmd"; doctype="pandoc2pdf", pandoc_options=["--toc", "--toc-depth= 1", "--number-sections", "--self-contained"])
+
+
+
