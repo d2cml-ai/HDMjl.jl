cf9f3027095ad27340e9411be25aed1131bdbbd9
Franco Caceres Vasquez <franco.caceres@pucp.edu.pe>
2022-10-18 19:26:20 -0500

    #27

--- src/HDMjl.jl
+++ src/HDMjl.jl
@@ -1,6 +1,6 @@
 module HDMjl
 
-export lambdaCalculation, init_values, as_logical, LassoShooting_fit, rlasso, rlassoEffect, rlassoEffects, rlassoIVselectX, rlassoIVselectZ, rlassoIV, rlassologit, rlassologitEffect, rlassologitEffects, rlassoATE, rlassoATET, rlassoLATE, rlassoLATET, tsls, r_summary, r_print, r_confint, r_predict, get_data
+export lambdaCalculation, init_values, as_logical, LassoShooting_fit, rlasso, rlassoEffect, rlassoEffects, rlassoIVselectX, rlassoIVselectZ, rlassoIV, rlassologit, rlassologitEffect, rlassologitEffects, rlassoATE, rlassoATET, rlassoLATE, rlassoLATET, tsls, r_summary, r_print, r_confint, r_predict, get_data, ginv
 
 using Statistics, GLM, DataFrames, LinearAlgebra, GLMNet, Random, PrettyTables, Distributions, CSV, RData, CodecXz
 
