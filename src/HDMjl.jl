f02745e45d25c4177999b8292377ffa8c1383d52
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2022-10-19 12:51:12 -0500

    exponential formula into dataframe

--- src/HDMjl.jl
+++ src/HDMjl.jl
@@ -2,7 +2,7 @@ module HDMjl
 
 export lambdaCalculation, init_values, as_logical, LassoShooting_fit, rlasso, rlassoEffect, rlassoEffects, rlassoIVselectX, rlassoIVselectZ, rlassoIV, rlassologit, rlassologitEffect, rlassologitEffects, rlassoATE, rlassoATET, rlassoLATE, rlassoLATET, tsls, r_summary, r_print, r_confint, r_predict, get_data, data_formula
 
-using Statistics, GLM, DataFrames, LinearAlgebra, GLMNet, Random, PrettyTables, Distributions, CSV, RData, CodecXz, StatsModels, StatsBase
+using Statistics, GLM, DataFrames, LinearAlgebra, GLMNet, Random, PrettyTables, Distributions, CSV, RData, CodecXz, StatsModels, StatsBase, Combinatorics
 
 include("help_functions.jl")
 include("LassoShooting_fit.jl")
--- src/formula.jl
+++ src/formula.jl
@@ -13,6 +13,43 @@ function data_formula(frml::FormulaTerm, data::DataFrame)
     return (res, x_df)
     # return coef_names
 end
+
+function data_formula(;x_names::Array{String} = nothing, y_name::String = nothing, Data = nothing, upto::Int = 2, intercept::Bool = false)
+    # ref: https://stackoverflow.com/questions/74117969/statsmodels-jl-exponential-formulas-formulay-x1-x2-x3-x4-x52-in/74118944#74118944
+    """
+    return a dataframe of covariates
+  
+    - `R`: model.matrix(y ~ (x1 + x3 + s9 + exp3)^2, data = df)
+    - `Julia`: data_formula(["x1", "x2", "s9", "exp3"], y_name = "y", data = df, intercept = false, upto = 2)
+  
+    """
+  
+    xs = term.(Symbol.(x_names))
+  
+    subsets(X; from=0, upto=upto) =
+    Iterators.flatten(combinations(X,i) for i=max(0,from):min(upto,length(X)))
+  
+  
+    term_vec = collect(subsets(xs; from = 1, upto = upto))
+  
+    rhs = map(x -> reduce(&, x), term_vec)
+    rhs_names = vcat([join(string.(x),'*') for x in term_vec])
+  
+    x1 = ModelMatrix(ModelFrame(FormulaTerm(Term(Symbol(y_name)),Tuple(rhs)), Data)).m
+  
+    if intercept
+      x1 = x1
+      rhs_names = vcat("(Intercept)", rhs_names)
+    else
+      x1 = x1[:, Not(1)]
+      rhs_names = rhs_names
+    end
+    
+    x_df = DataFrame(x1, rhs_names)
+  
+    return x_df
+  end
+
 # using StatsModels, StatsBase, DataFrames, StableRNGs; rng = StableRNG(1);
 
 
