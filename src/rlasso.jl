50bcb795829e5a3d2420fd58880096ab3be3717c
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2022-10-19 09:31:39 -0500

    full formula

--- src/rlasso.jl
+++ src/rlasso.jl
@@ -199,11 +199,11 @@ function rlasso(x, y; post = true, intercept = true, model = true,
 end
 
 
-function rlasso(formula::FormulaTerm, Data::DataFrame;)# post = true, intercept = true, model = true, homoskedastic = false, X_dependent_lambda = false, lambda_start = nothing, c = 1.1, maxIter = 15, tol::Float64 = 1e-5, n = size(Data, 1) gamma = 0.1 / log(n), threshold = nothing)
+function rlasso(formula::FormulaTerm, Data::DataFrame; post = true, intercept = true, model = true, homoskedastic = false, X_dependent_lambda = false, lambda_start = nothing, c = 1.1, maxIter = 15, tol::Float64 = 1e-5, n = size(Data, 1) gamma = 0.1 / log(n), threshold = nothing)
 
     y, x = data_formula(formula, Data)
     
-    rlasso(x, y;)# post = post, intercept = intercept, model = model, homoskedastic = homoskedastic, X_dependent_lambda = X_dependent_lambda, lambda_start = lambda_start, c = c, maxIter = maxIter, tol = tol, n = size(y, 1), gamma = gamma, threshold = threshold)
+    rlasso(x, y; post = post, intercept = intercept, model = model, homoskedastic = homoskedastic, X_dependent_lambda = X_dependent_lambda, lambda_start = lambda_start, c = c, maxIter = maxIter, tol = tol, n = size(y, 1), gamma = gamma, threshold = threshold)
 end
 
 function r_summary(rlasso_obj::Dict; all = false)
